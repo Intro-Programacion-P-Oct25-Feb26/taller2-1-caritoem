17e9e01b042bbfa1cea5ba4e132d3012ef03e6eb
caritoem <caritomazonespinosa@gmail.com>
2025-10-16 12:55:09 -0500

    hola

--- proyecto03/src/proyecto03/Proyecto03.java
+++ proyecto03/src/proyecto03/Proyecto03.java
@@ -15,6 +15,7 @@ public class Proyecto03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    } System.out.println("GitBush usando Java");
+    
     
 }
