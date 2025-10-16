8b2768b6efdc1345f511e00342543dd699aed242
caritoem <caritomazonespinosa@gmail.com>
2025-10-16 12:47:38 -0500

    cualquier nombre

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,6 +15,6 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    }System.out.printnl("Paris L/N");)
     
 }
